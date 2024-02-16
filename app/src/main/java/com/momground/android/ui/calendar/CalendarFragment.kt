55dc3c682637d1a30fc2ab6f87588fc020628134
heejaedev <heejaedev@gmail.com>
2024-02-17 05:13:08 +0900

    Feat: Add CalendarFragment

---
+++ app/src/main/java/com/momground/android/ui/calendar/CalendarFragment.kt
@@ -0,0 +1,38 @@
+package com.momground.android.ui.calendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.momground.android.databinding.FragmentCalendarBinding
+import com.momground.android.databinding.FragmentHomeBinding
+
+class CalendarFragment : Fragment() {
+
+    private var _binding: FragmentCalendarBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this)[CalendarViewModel::class.java]
+
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
