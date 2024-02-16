99187ca88d1b7241bf20386cfc927f94b3eb6d51
heejaedev <heejaedev@gmail.com>
2024-02-17 04:06:39 +0900

    Update: Bottom NavigationBar #3

---
+++ app/src/main/java/com/momground/android/ui/health/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.momground.android.ui.health
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
