cbc03a9e7314d1f9ab0a0008c2b13ed9810f6042
heejaedev <heejaedev@gmail.com>
2024-02-17 05:13:43 +0900

    Feat: Add HomeFragment

---
+++ app/src/main/java/com/momground/android/ui/home/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.momground.android.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.momground.android.databinding.FragmentHomeBinding
+import com.momground.android.ui.calendar.CalendarViewModel
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this)[CalendarViewModel::class.java]
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
