643d7278b00470f5a82d2daad80e34000d9a7d19
heejaedev <heejaedev@gmail.com>
2024-02-17 04:04:02 +0900

    Feat: MyPage init

---
+++ app/src/main/java/com/momground/android/ui/mypage/MyPageViewModel.kt
@@ -0,0 +1,13 @@
+package com.momground.android.ui.mypage
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyPageViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Mypage Fragment"
+    }
+    val text: LiveData<String> = _text
+}
