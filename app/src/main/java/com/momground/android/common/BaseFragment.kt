098f803d2abed79fdbe66a5ce4888dcd33abfaf4
heejaedev <heejaedev@gmail.com>
2024-02-18 05:28:26 +0900

    Feat: ready for analytics

--- app/src/main/java/com/momground/android/common/BaseFragment.kt
+++ app/src/main/java/com/momground/android/common/BaseFragment.kt
@@ -1,5 +1,7 @@
 package com.momground.android.common
 
+import android.os.Bundle
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import com.momground.android.data.User
 
@@ -9,5 +11,15 @@ open class BaseFragment: Fragment(){
     }
     open fun isNoneLogin(): Boolean{
         return !User.isLogin
+    }
+
+    protected open fun sendEvent(s: String?, @Nullable bundle: Bundle?): BaseFragment {
+        // Analytics.send { analytics -> analytics.logEvent(s, bundle) }
+        return this
+    }
+
+    protected open fun sendEvent(s: String?): BaseFragment {
+        // Analytics.send { analytics -> analytics.logEvent(s, null) }
+        return this
     }
 }
