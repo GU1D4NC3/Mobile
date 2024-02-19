af331a7d5816245496dfd0d103f435e6bed40916
heejaedev <heejaedev@gmail.com>
2024-02-20 00:40:09 +0900

    Feat: add NetworkConfig

---
+++ app/src/main/java/com/momground/android/common/LocalNetworkConfig.java
@@ -0,0 +1,12 @@
+package com.momground.android.common;
+
+/**
+ * 흠....
+ * 여기에서 URL을 설정하고..
+ * 푸시할때는 제외해주면 되겠네요..
+ */
+
+public class LocalNetworkConfig {
+    // dev variant는 스테이징으로 고정해놓도록 해요.
+    public static final String BASE_URL = "https://test.accx.dev/";
+}
